cd3f7b089543ae47760d486643d9f684a0f4ebdd
Rohith1034 <109918118+Rohith1034@users.noreply.github.com>
2025-07-27 11:38:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.getElementsByName("submit_btn");
+
+  if (elements.length > 0) {
+    elements[0].addEventListener("click", () => {
+      const firstName = document.getElementsByName("first_name")[0]?.value || "";
+      const lastName = document.getElementsByName("last_name")[0]?.value || "";
+      const phone = document.getElementsByName("phone_no")[0]?.value || "";
+      const emailId = document.getElementsByName("email")[0]?.value || "";
+
+      const str = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${emailId}`;
+      
+      alert(str);
+    });
+  }
+});
